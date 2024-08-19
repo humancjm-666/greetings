09ad373a3fe3c583e9370b5ada6f7d4c127f6a59
humancjm-666 <82277841@qq.com>
2024-08-19 15:29:45 +0800

    更新

--- greetings.go
+++ greetings.go
@@ -1,9 +1,16 @@
 package greetings
 
-import "fmt"
+import (
+    "errors"
+    "fmt"
+)
+func Hello(name string) (string, error) {
+    // 如果没有给出名字，返回一个带有消息的错误。
+    if name == "" {
+        return "", errors.New("empty name")
+    }
 
-func Hello(name string) string {
     var message string
     message = fmt.Sprintf("Hi, %v. Welcome!", name)
-    return message
+    return message, nil
 }
