2110cae7966c353015b4bca1f35fd7ba25405368
humancjm-666 <82277841@qq.com>
2024-08-19 17:33:35 +0800

    更新

--- greetings.go
+++ greetings.go
@@ -3,7 +3,10 @@ package greetings
 import (
     "errors"
     "fmt"
+    "math/rand"
+    "time"
 )
+
 func Hello(name string) (string, error) {
     // 如果没有给出名字，返回一个带有消息的错误。
     if name == "" {
@@ -13,4 +16,21 @@ func Hello(name string) (string, error) {
     var message string
     message = fmt.Sprintf("Hi, %v. Welcome!", name)
     return message, nil
+}
+
+// init 为函数中使用的变量设置初始值。
+func init() {
+    rand.Seed(time.Now().UnixNano())
+}
+
+// randomFormat 返回一组问候消息中的一个。返回的
+// 消息是随机选择的.
+func randomFormat() string {
+    // 消息格式的切片
+    formats := [] string {
+        "Hi, %v. Welcome!",
+        "Great to see you, %v!",
+        "Hail, %v! Well met!",
+    }
+    return formats[rand.Intn(len(formats))]
 }
