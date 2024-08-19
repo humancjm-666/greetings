a9c0dfc85e3e85e0c9d0d2f5fbaf40f7d2376e1a
humancjm-666 <82277841@qq.com>
2024-08-19 15:40:16 +0800

    更新

--- greetings.go
+++ greetings.go
@@ -9,7 +9,7 @@ func Hello(name string) (string, error) {
     if name == "" {
         return "", errors.New("empty name")
     }
-    
+
     var message string
     message = fmt.Sprintf("Hi, %v. Welcome!", name)
     return message, nil
