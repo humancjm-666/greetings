abb8b9629653a5cc3f63abc0fff1eb5b376e006e
humancjm-666 <82277841@qq.com>
2024-08-22 17:42:09 +0800

    更新greetings

--- greetings.go
+++ greetings.go
@@ -10,12 +10,28 @@ import (
 func Hello(name string) (string, error) {
     // 如果没有给出名字，返回一个带有消息的错误。
     if name == "" {
-        return "", errors.New("empty name")
+        return name, errors.New("empty name")
     }
 
     var message string
     message = fmt.Sprintf(randomFormat(), name)
     return message, nil
+}
+
+// Hellos 返回一个 map，该地图将每个已命名的人员 与问候消息相关联。
+func Hellos(names [] string) (map[string] string, error) {
+    // 将名称与消息关联的map.
+    messages := make(map[string] string)
+    // 遍历接收到到名称切片，调用 Hello 函数为每个名字获取一条消息。
+    for _, name := range names {
+        message, err := Hello(name)
+        if err != nil {
+            return nil, err
+        }
+        // 在map 中，将检索到到消息与名称相关联。
+        messages[name] = message
+    }
+    return messages, nil
 }
 
 // init 为函数中使用的变量设置初始值。
